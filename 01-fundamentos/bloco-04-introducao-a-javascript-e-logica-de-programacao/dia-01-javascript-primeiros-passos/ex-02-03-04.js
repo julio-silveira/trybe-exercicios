32c19a7bb5bc04879b34cdfbccfba6624d6930be
julio-silveira <julioqueiroz15@gmail.com>
2022-06-22 16:36:02 -0300

    Exercicio 02 concluido

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/ex-02-03-04.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 10;
+const c = 15;
+
+if(a>b){
+    console.log("O maior número é entre", a ," e ", b, " é:", a)
+}
+else{
+    console.log("O maior número é entre", a ," e ", b, " é:", b)
+}
